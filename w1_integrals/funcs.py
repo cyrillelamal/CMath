e8ad38dc52a0af93f25fb3b457c06a56bba2aec8
Cyrille <cyrillelamal@gmail.com>
2019-09-15 19:00:53 +0300

    refactored

--- w1_integrals/funcs.py
+++ w1_integrals/funcs.py
@@ -3,61 +3,71 @@ import math
 from decorators import with_steps, with_print
 
 
-STEPS = [10**2, 10**3, 10**4]
+PRECISION = 10**(-5)
+
+NUMBER_OF_STEPS = [10**2, 10**3, 10**4]
+
+BASE_NUMBER_OF_STEPS = 10  # Variable
+
 A = 0
 B = 1
 
 
 def func(x):
     """Integrated function"""
-    # return math.exp(-x**2)
-    return math.sin(x)
+    return math.sin(x)  # 0.45970
 
 
-@with_steps(STEPS)
-@with_print
-def left_triangles(n):
-    h = (B - A) / n  # h
+def left_rectangles_generator(h):
     x = A
-    i = 0  # sum
     while x < B:
-        i += func(x)
+        yield func(x)
         x += h
-    i *= h
-    return i, n
 
 
-@with_steps(STEPS)
-@with_print
-def right_triangles(n):
-    h = (B - A) / n  # h
+def right_rectangles_generator(h):
     x = A + h
-    i = 0  # sum
     while x <= B:
-        i += func(x)
+        yield func(x)
         x += h
-    i *= h
+
+
+def trapezium_generator(h):
+    x = A + h
+    while x < B - h:
+        yield func(x)
+        x += h
+
+
+@with_steps(NUMBER_OF_STEPS)
+@with_print
+def left_rectangles(n):
+    h = (B - A) / n  # h
+    i = h * sum(left_rectangles_generator(h))
     return i, n
 
 
-@with_steps(STEPS)
+@with_steps(NUMBER_OF_STEPS)
+@with_print
+def right_rectangles(n):
+    h = (B - A) / n  # h
+    i = h * sum(right_rectangles_generator(h))
+    return i, n
+
+
+@with_steps(NUMBER_OF_STEPS)
 @with_print
 def trapezium(n):
     h = (B - A) / n  # h
-    sum_ = 0
-    x = A + h
-    while x <= B - h:
-        sum_ += func(x)
-        x += h
-    i = h * ((func(A) + func(B)) / 2 + sum_)
+    i = h * ((func(A) + func(B)) / 2 + sum(trapezium_generator(h)))
     return i, n
 
 
-@with_steps(STEPS)
+@with_steps(NUMBER_OF_STEPS)
 @with_print
 def parable(n):  # Simpson
     if n % 2 == 1:
-        raise ValueError('N must be even')
+        raise ValueError('Number of steps must be even')
     sum_ = func(A) + func(B)
     h = (B - A) / (2*n)
 
@@ -70,12 +80,29 @@ def parable(n):  # Simpson
     for i in range(1, n+1):
         even_sum += func(h*(-1+2*i)+A)
     sum_ += 4*even_sum
-    i = sum_ * (h/3)
-    return i, n
+    sum_ *= (h/3)
+    return sum_, n
 
 
+@with_print
 def alg1():
-    print('will be soon 1')
+    """Count at every step until the precision is attempted"""
+    base_h = (B - A) / BASE_NUMBER_OF_STEPS
+    # A smaller integral in the integral
+    previous_sum = 0
+    h = base_h
+
+    # Base small integral (do, while)
+    current_sum = h * sum(left_rectangles_generator(h))
+
+    while abs(current_sum - previous_sum) > PRECISION:
+        # Integrate with a new step
+        previous_sum = current_sum
+        h /= 2
+
+        current_sum = h * sum(left_rectangles_generator(h))
+
+    return current_sum, h
 
 
 def alg2():
--- w1_integrals/w1_integrals.py
+++ w1_integrals/w1_integrals.py
@@ -3,51 +3,41 @@ import sys
 from FiniteConsole.FiniteConsole import Program, Menu, Option
 import funcs as f
 
+p = Program()
+p.init_menu = Menu('main')
+p.init_menu.append_options(
+    Option(1, 'const_step', 'Методы с постоянным шагом.'),
+    Option(2, 'var_step', 'Методы с переменным шагом.'),
+    Option(3, 'exit', 'Выйти из программы.')
+)
 
-# Main menu1
-main_menu = Menu('main')
-main_menu.append_option(Option('1', 'const_step', 'Методы с постоянным шагом'))
-main_menu.append_option(Option('2', 'var_step', 'Методы с переменным шагом'))
-main_menu.append_option(Option('3', 'exit', 'Выйти из программы'))
+# Const steps
+Menu('const_step').append_options(
+    Option('1', 'left_t', 'Метод левых прямоугольников'),
+    Option('2', 'right_t', 'Метод правых прямоугольников'),
+    Option('3', 'trap', 'Метод трапеций'),
+    Option('4', 'parabola', 'Метод парабол'),
+    Option('5', 'main', 'В главное меню')
+)
+# Var steps
+Menu('var_step').append_options(
+    Option('1', 'alg_1', 'Алгоритм 1'),
+    Option('2', 'alg_2', 'Алгоритм 2'),
+    Option('3', 'main', 'В главное меню')
+)
 
-
-# Exit (menu)
-exit_menu = Menu('exit', lambda: sys.exit())
-
-
-# Const steps menu
-const_step_menu = Menu('const_step')
-const_step_menu.append_option(Option('1', 'left_t', 'Метод левых прямоугольников'))
-const_step_menu.append_option(Option('2', 'right_t', 'Метод правых прямоугольников'))
-const_step_menu.append_option(Option('3', 'trap', 'Метод трапеций'))
-const_step_menu.append_option(Option('4', 'parabola', 'Метод парабол'))
-const_step_menu.append_option(Option('5', 'main', 'В глвное меню'))
-
-left_t = Menu('left_t', f.left_triangles)  # Left triangles
-right_t = Menu('right_t', f.right_triangles)  # Left triangles
-trap = Menu('trap', f.trapezium)  # Trapezium
-para = Menu('parabola', f.parable)  # Parable
-
-
-# Var steps menu
-var_step_menu = Menu('var_step')
-var_step_menu.append_option(Option('1', 'alg_1', 'Алгоритм 1'))
-var_step_menu.append_option(Option('2', 'alg_2', 'Алгоритм 2'))
-var_step_menu.append_option(Option('3', 'main', 'В глвное меню'))
-
-alg1 = Menu('alg_1', f.alg1)
-alg2 = Menu('alg_2', f.alg2)
-
-
-# PROGRAM
-# MAIN
-prog = Program(main_menu)
-# EXIT
-prog.append_menu(exit_menu)
-# CONST
-prog.append_menu(const_step_menu).append_menu(left_t).append_menu(right_t).append_menu(trap).append_menu(para)
-# VAR
-prog.append_menu(var_step_menu).append_menu(alg1).append_menu(alg2)
+# Finite states
+# Exit
+Menu('exit', lambda: sys.exit())
+# Const steps
+Menu('left_t', f.left_rectangles)
+Menu('right_t', f.right_rectangles)
+Menu('trap', f.trapezium)
+Menu('parabola', f.parable)
+# Var steps
+Menu('alg_1', f.alg1)
+Menu('alg_2', f.alg2)
 
 
-prog.loop()
+if __name__ == '__main__':
+    p.start_loop()
