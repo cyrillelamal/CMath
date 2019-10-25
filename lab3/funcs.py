46375760145ded548fe3d302351f1ab5830ba2cc
Cyrille <cyrillekossyguine@gmail.com>
2019-10-25 18:27:03 +0300

    w3 rk

--- lab3/funcs.py
+++ lab3/funcs.py
@@ -3,13 +3,14 @@ import math
 
 A = 0
 B = 1
+
 X0 = A
 Y0 = 1
 
 NUM_OF_SEP = [10, 50, 150]
 
 
-def equation(x, y):
+def func(x, y):
     return y * (1 - x)
 
 
@@ -21,10 +22,23 @@ def euler():
         print(f'Шаг интегрирования: {h}')
         print(f'x0={x:.5f}; y0={y:.5f}')
         for _ in range(n):
-            y += h * equation(x, y)
+            y += h * func(x, y)
             x += h
-            print(f'{y:.5f}')
+            print(f'{x:.5f}; {y:.5f}')
 
 
-def runge():
-    pass
+def rk():
+    for n in NUM_OF_SEP:
+        h = (B - A) / n
+        x = X0
+        y = Y0
+        print(f'Шаг интегрирования: {h}')
+        print(f'x0={x:.5f}; y0={y:.5f}')
+        for i in range(n):
+            k1 = h * func(x, y)
+            k2 = h * func(x+h/2, y+k1/2)
+            k3 = h * func(x+h/2, y+k2/2)
+            k4 = h * func(x+h, y+k3)
+            y += ((k1 + 2*k2 + 2*k3 + k4) / 6)
+            x += h
+            print(f'{x:.5f}; {y:.5f}')
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -8,12 +8,12 @@ p.init_menu = main
 
 main.append_options(
     Option(1, 'euler', 'Метод Эйлера'),
-    Option(2, 'runge', 'Метод Рунге-Кутта'),
+    Option(2, 'rk', 'Метод Рунге-Кутта'),
     Option(3, 'exit', 'Выйти')
 )
 
 Menu('euler', f.euler)
-Menu('runge', f.runge)
+Menu('rk', f.rk)
 Menu('exit', lambda: exit())
 
 
